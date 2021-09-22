9d96638c15cb1dd82b9fd387a25ae27ff6e2d76b
aisha dorsett <aishadorsett23@gmail.com>
2021-09-22 17:11:09 +0100

    finished chapter 12

--- lib/dockingstation.rb
+++ lib/dockingstation.rb
@@ -1,15 +1,12 @@
 class DockingStation
     attr_reader :bike
 
-    def initialize
-        @bikes
-    end
-
     def release_bike
+        fail 'no bikes available' unless @bike
         @bike
     end
 
-    def docked?(bike)
+    def docked(bike)
         @bike = bike
     end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,16 +3,26 @@ require 'dockingstation'
 describe DockingStation do
    describe '#release_bike' do
       it 'can release a bike' do
-         expect(DockingStation.new).to respond_to :release_bike
+         bike = Bike.new
+         subject.docked(bike)
+         expect(subject.release_bike).to eq bike
+      end
+   end
+         
+   describe 'release bike' do
+      it 'returns error when no bikes are docked' do
+         expect {subject.release_bike}.to raise_error 'no bikes available'
       end
    end
    
    describe 'docked bike' do
       it 'return the docked bike' do 
          bike = Bike.new
-         expect(subject.docked?(bike)).to eq(bike)
+         expect(DockingStation.new.docked(bike)).to eq bike
       end
    end
+
+  
 
    # describe 'view docked bikes' do
    #    it 'sees whether docking station has docked bike' do
